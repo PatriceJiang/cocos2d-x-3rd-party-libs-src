b2c056479f7eb55e80bdc51108839e6f261929d4
patricejiang <jiangzhan@chukong-inc.com>
2018-10-31 17:13:23 +0800

    update script

--- build/copy_ssl_to_lite_external.sh
+++ build/copy_ssl_to_lite_external.sh
@@ -6,5 +6,5 @@ dst=~/Github/cocos2d-x-lite/external/android
 for arch in armeabi-v7a arm64-v8a x86 ; 
 do 
 	cp -rv $src/prebuilt/$arch/*.a $dst/$arch/
-	cp -v  $src/include/openssl    $dst/$arch/incllude/
+	cp -r  $src/include/openssl    $dst/$arch/include/
 done
